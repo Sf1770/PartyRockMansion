917b4581cfd7f4e93142b0dad7ab3868d44ad248
Sf1770 <sf1770@nyu.edu>
2017-08-24 16:30:30 -0400

    Finished Party Rock Project

---
+++ PartyRockMansion/Model/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRockMansion
+//
+//  Created by Sabrina Fletcher on 8/16/17.
+//  Copyright © 2017 Sabrina Fletcher. All rights reserved.
+//
+
+import Foundation
---
+++ PartyRockMansion/View/PartyCell.swift
@@ -0,0 +1,22 @@
+//
+//  PartyCellTableViewCell.swift
+//  PartyRockMansion
+//
+//  Created by Sabrina Fletcher on 8/14/17.
+//  Copyright © 2017 Sabrina Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class PartyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    func updateUI() {
+        
+    }
+
+
+}
